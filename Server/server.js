2a21556048984839b4af6899a624ffd2ee8b81cd
AndreaHK5 <cremese@gmail.com>
2015-11-14 17:33:23 -0800

    refactored for persistence layer

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,17 @@
 
 // Main dependency injections
 var votesPath = "./Storage/votes.json";
-var storage = require('./Modules/Storage.js').call({}, votesPath);
-var votesController = require('./Modules/VotesController.js').call({}, storage);
+var persistanceLayer = require('./Modules/persistanceLayer.js').call({}, votesPath);
+var votesController = require('./Modules/VotesController.js').call({}, persistanceLayer);
 
 // start app with dependency injection
 require('./app.js').call({}, 3000, votesController);
 
 
 //TODO:
-// Include Persistance layer to MongoLab ILO file system
-// TODO manual mocks, for the time being. Must find a package like Moq for node
+// Include Persistance layer to MongoLab ILO file system for vote file
+// Include storage of all votes (with time stamps)
+// manual mocks, for the time being. Must find a package like Moq for node
 // Split Controller from business logic for better unit testability
 // find better package for assertions
 // End to end test.

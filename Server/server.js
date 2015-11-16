7c1c74670273db045026039bedbae4976961de25
AndreaHK5 <cremese@gmail.com>
2015-11-16 11:21:31 -0800

    updated TODO list

--- Server/server.js
+++ Server/server.js
@@ -11,6 +11,7 @@ require('./app.js').call({}, 3000, votesController);
 //TODO:
 // Include Persistance layer to MongoLab ILO file system for vote file
 // Include storage of all votes (with time stamps)
+// change naming convetion for app and server
 // manual mocks, for the time being. Must find a package like Moq for node
 // Split Controller from business logic for better unit testability
 // find better package for assertions

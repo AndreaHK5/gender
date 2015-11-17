3cfd53feab0eec843ca9f1a02b82f7d8d3085c0e
ACremese <andrea.cremese@finoconsulting.com>
2015-11-17 08:46:23 -0800

    updated read me file and server package.json

--- Server/server.js
+++ Server/server.js
@@ -8,13 +8,3 @@ var votesController = require('./Modules/VotesController.js').call({}, persistan
 require('./app.js').call({}, 3000, votesController);
 
 
-//TODO:
-// Include End to End test on temp vote file
-// Include Persistance layer to MongoLab ILO file system for vote file
-// Include storage of all votes (with time stamps)
-// change naming convetion for app and server
-// manual mocks, for the time being. Must find a package like Moq for node
-// Split Controller from business logic for better unit testability
-// find better package for assertions
-// End to end test.
-

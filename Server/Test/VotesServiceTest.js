fdb6f040294abdeacd9323be036c78f24bf453f4
ACremese <andrea.cremese@finoconsulting.com>
2015-11-20 18:12:30 -0500

    functioning service tests up and running

--- Server/Test/VotesServiceTest.js
+++ Server/Test/VotesServiceTest.js
@@ -1,14 +1,32 @@
+var assert = require('assert');
+
 describe("Votes Business logic test", function(){
 
+  function init(votes){
+      var _storageMock = {
+        votes : votes,
+        Update : function(newObj) { this.votes = newObj},
+        Read : function(){ return this.votes},
+      };    
+      return require('../Modules/VotesService.js').call({}, _storageMock);
+  };
 
-  beforeEach( function(next){
-
-    var _sut = require('./Modules/VotesService.js').call({}, persistanceMock);
-    next();
-  });
 
   it("adds male votes", function(){
+    var votes = { male:0, female:0 };
+    var _sut = init(votes);
+    _sut.addVote("male");
+    assert.equal(votes.male, 1);
+    assert.equal(votes.female, 0);     
+  });
 
+  it("adds female votes", function(){
+    var votes = { male:0, female:0 };
+    var _sut = init(votes);
+
+    _sut.addVote("female");
+    assert.equal(votes.female, 1);     
+    assert.equal(votes.male, 0);     
   });
 
 });

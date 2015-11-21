df9cd752db4fec74d23ec8548363336e62143f41
ACremese <andrea.cremese@finoconsulting.com>
2015-11-20 20:37:31 -0500

    End to end test updated with new architecture

--- Server/Test/VotesEndToEndTest.js
+++ Server/Test/VotesEndToEndTest.js
@@ -6,20 +6,19 @@ var fs = require('fs');
 describe("Votes Controller Unit Test", function () {
   var tempStorageFile  = "./tempVotes.json";
   var votes;
-  var _storageMock;
 
   // setup
   beforeEach(function (next) {
     fs.readFile(tempStorageFile, function (err, data){
       if (!err) {fs.unlink(tempStorageFile);}
     // Injection of mocks
-      _storageMock = require('../Modules/PersistanceLayer.js').call({}, tempStorageFile);
+      var _storageMock = require('../Modules/PersistanceLayer.js').call({}, tempStorageFile);
+      var votesService = require('../Modules/VotesService.js').call({}, _storageMock);
+      var votesController = require('../Modules/VotesController.js').call({}, votesService);
 
-      // instantiation of system under test
-      var _sut = require("../Modules/VotesController").call({},_storageMock);
 
       // TODO find out if there is another mock for servers rather than the hand made one.
-      express = require('../app').call({},5000, _sut);
+      express = require('../app').call({},5000, votesController);
       app = express.app;
       server = express.server;
       next();
@@ -28,43 +27,43 @@ describe("Votes Controller Unit Test", function () {
   });
 
   // teardown
-  afterEach(function () {
+  afterEach(function (done) {
       // delete file
       fs.readFile(tempStorageFile, function (err, data){
         if (!err) {fs.unlink(tempStorageFile);}
         server.close();
+        done();
       });
   });
 
-  // it("cast vote, read vote and reset votes", function(done){
-  //   request(app)
-  //     .put('/api/vote')
-  //     .send({gender : 'male'})
-  //     .expect(assertionPut)
-  //     .then(getTest);
+  it("cast vote, read vote and reset votes", function(done){
+    request(app)
+      .put('/api/vote')
+      .send({gender : 'male'})
+      .expect(assertionPut)
+      .then(getTest);
 
-  //     function assertionPut(res) {
+      function assertionPut(res) {
 
-  //       if (res.status != 200) {throw new Error("expected 200, received " + res.status);}
-  //       console.log(tempStorageFile);
-  //       fs.readFile(tempStorageFile, function (err, data) {
-  //         if (err) {throw new Error("File not created by persistance layer");}
-  //         var votesRead = JSON.parse(data);
-  //         if (votesRead.male != 1 ) {throw new Error("votes not updated in persistance layer - male, expected 1, actual " + data);}
-  //         if (votesRead.female != 0 ) {throw new Error("votes not updated in persistance layer - female");}
-  //       });
-  //     }
+        if (res.status != 200) {throw new Error("expected 200, received " + res.status);}
+        fs.readFile(tempStorageFile, function (err, data) {
+          if (err) {throw new Error("File not created by persistance layer");}
+          var votesRead = JSON.parse(data);
+          if (votesRead.male != 1 ) {throw new Error("votes not updated in persistance layer - male, expected 1, actual " + data);}
+          if (votesRead.female != 0 ) {throw new Error("votes not updated in persistance layer - female");}
+        });
+      }
 
-  //     function getTest(){
-  //       request(app)
-  //         .get('/api/votes')
-  //         .set('Accept', 'application/json')
-  //         .expect(200, { male : 1, female : 0})
-  //         .end(function(err, res){
-  //           if (err) { return done(err);}
-  //           done();
-  //         });
-  //     }
-  // })
+      function getTest(){
+        request(app)
+          .get('/api/votes')
+          .set('Accept', 'application/json')
+          .expect(200, { male : 1, female : 0})
+          .end(function(err, res){
+            if (err) { return done(err);}
+            done();
+          });
+      }
+  })
 
 });

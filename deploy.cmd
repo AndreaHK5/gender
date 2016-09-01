8a6839eabcd72fdce851067d3b37f4f1c96166fc
andrea <cremese@gmail.com>
2016-08-31 18:18:27 -0700

    updated deploy commands

--- deploy.cmd
+++ deploy.cmd
@@ -1,6 +1,11 @@
-if EXIST "%DEPLOYMENT_TARGET%\bower.json" (
-    pushd "%DEPLOYMENT_TARGET%"
-    call :ExecuteCmd bower install
-    IF !ERRORLEVEL! NEQ 0 goto error
-    popd
+IF EXIST "%DEPLOYMENT_TARGET%\Client\bower.json" (
+
+pushd "%DEPLOYMENT_TARGET%\Client"
+
+call ..\node_modules\.bin\bower install
+
+IF !ERRORLEVEL! NEQ 0 goto error
+
+popd
+
 )
